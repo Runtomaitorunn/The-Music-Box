edea16a142ad904086e6bb7472c2d59e5f2d0a6d
Mai <yeahmy1022@gmail.com>
2025-03-15 14:48:43 +0000

    add multiple renderer in highlight color target

--- TheMusicBox/Assets/Scripts/InteractablePrefabs/InteractableFeedbackManager.cs
+++ TheMusicBox/Assets/Scripts/InteractablePrefabs/InteractableFeedbackManager.cs
@@ -1,14 +1,21 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using static InteractableFeedbackManager;
 
 
 public class InteractableFeedbackManager : MonoBehaviour
 {
+    [System.Serializable]
+    public struct RendererData
+    {
+        public MeshRenderer renderer;
+        public int materialIndex;
+        public Color defaultColor;
+    }
     [Header("Highlight Outline")]
-    public MeshRenderer outlineRenderer;
-    public int materialIndex = 1;
-    [SerializeField]
-    private Color outlineDefaultColor;
+    public List<RendererData> rendererDatas;
     public Color outlineHighlightColor;
 
     [Header("Play Loading Circular Anim")]
@@ -16,32 +23,53 @@ public class InteractableFeedbackManager : MonoBehaviour
 
     public void Start()
     {
-        if (outlineRenderer != null && outlineRenderer.materials.Length > materialIndex)
+        for (int i = 0; i < rendererDatas.Count; i++)
         {
-            outlineDefaultColor = outlineRenderer.materials[materialIndex].GetColor("_Mix_Color");
+            if (rendererDatas[i].renderer != null && rendererDatas[i].renderer.materials.Length > rendererDatas[i].materialIndex)
+            {
+                RendererData data = rendererDatas[i];
+                data.defaultColor = data.renderer.materials[data.materialIndex].GetColor("_Mix_Color");
+                rendererDatas[i] = data; // Update the list with the default color
+            }
+            else
+            {
+                Debug.LogError("Renderer is missing or material index is out of range for renderer at index " + i);
+            }
         }
     }
+
 
     #region Highlight Outline
     public void HighlightOutlineColor()
     {
-        if (outlineRenderer != null && outlineRenderer.materials.Length > materialIndex)
+        foreach (var data in rendererDatas)
         {
-            Material targetMaterial = outlineRenderer.materials[materialIndex];
-
-            targetMaterial.SetColor("_Mix_Color", outlineHighlightColor);
-        }
-        else
-        {
-            Debug.LogWarning("MeshRenderer is missing or material index is out of range.");
+            if (data.renderer != null && data.renderer.materials.Length > data.materialIndex)
+            {
+                Material targetMaterial = data.renderer.materials[data.materialIndex];
+                targetMaterial.SetColor("_Mix_Color", outlineHighlightColor);
+            }
+            else
+            {
+                Debug.LogWarning("Renderer is missing or material index is out of range for renderer " + data.renderer.name);
+            }
         }
     }
 
     public void RevertOutlineColor()
     {
-        Material targetMaterial = outlineRenderer.materials[materialIndex];
-
-        targetMaterial.SetColor("_Mix_Color", outlineDefaultColor);
+        foreach (var data in rendererDatas)
+        {
+            if (data.renderer != null && data.renderer.materials.Length > data.materialIndex)
+            {
+                Material targetMaterial = data.renderer.materials[data.materialIndex];
+                targetMaterial.SetColor("_Mix_Color", data.defaultColor);
+            }
+            else
+            {
+                Debug.LogWarning("Renderer is missing or material index is out of range for renderer " + data.renderer.name);
+            }
+        }
     }
     #endregion Highlight Outline
 
