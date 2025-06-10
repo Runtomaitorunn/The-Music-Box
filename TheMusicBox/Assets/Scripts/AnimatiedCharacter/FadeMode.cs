a8a8f6f777e483373786ac7ce063ebb598ba75bb
Mai <yeahmy1022@gmail.com>
2025-06-10 08:21:28 +0100

    serial fade transition + different transition mode

--- TheMusicBox/Assets/Scripts/AnimatiedCharacter/FadeMode.cs
+++ TheMusicBox/Assets/Scripts/AnimatiedCharacter/FadeMode.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
-public enum FadeMode
+public abstract class FadeMode : ScriptableObject
 {
-    FadeInOnly,
-    FadeOutOnly,
-    FadeInThenOut,
-    FadeOutThenIn
+    [SerializeField] protected FadeLerpExecutor executor;
+    public abstract void ApplyFade(
+        GameObject obj,
+        string objectId,
+        int signalTypeInt,
+        MonoBehaviour context,
+        ref bool fadeInReceived,
+        ref bool fadeOutReceived,
+        System.Action advanceToNext,
+        float fadeInDuration,
+        float fadeOutDuration);
 }
